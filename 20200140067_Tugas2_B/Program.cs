0025952ef2974238f046ad7d3c5d2258e77ce19c
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2022-03-25 04:10:22 -0700

    Menambahkan Data Kedalam Tabel Supplier dan Pelayan_Apotek

--- 20200140067_Tugas2_B/Program.cs
+++ 20200140067_Tugas2_B/Program.cs
@@ -14,13 +14,13 @@ namespace _20200140067_Tugas2_B
             SqlConnection con = null;
             try
             {
-                con = new SqlConnection(("data source=LAPTOP-O4QM7APE;database=Apotek_Tiara067;Integrated Security = TRUE"));
+                con = new SqlConnection("data source=LAPTOP-O4QM7APE;database=Apotek_Tiara067;Integrated Security = TRUE");
                 con.Open();
 
                 SqlCommand cm = new SqlCommand("create table Supplier (Kode_Supplier char(6) not null primary key, Nama_Supplier varchar(30) not null, No_Telpon char(12) not null, Alamat varchar (50))" +
                     "create table Pembeli (Kode_Pembeli char(6) not null primary key, Nama_Pembeli varchar(30) not null, Jenis_Kelamin char(1) CHECK (Jenis_Kelamin in ('L','P')), No_Telepon char(12), Alamat varchar(50))" +
                     "create table Obat (Kode_Obat char(6) not null primary key, Merek_Obat varchar(30) not null, Jenis_Obat varchar(30) not null, Kategori_Obat varchar(10), Harga_Obat money, Stok_Obat varchar(30), Tanggal_Produksi date, Tanggal_Kadaluwarsa date, Kode_Pelayan char(6))" +
-                    "create table Pelayan_Apotek (Kode_Pelayan char(6) not null primary key, Nama_Karyawan varchar(30) not null, Jenis_Kelamin char(1) CHECK(Jenis_Kelamin in ('L','P')))" +
+                    "create table Pelayan_Apotek (Kode_Pelayan char(6) not null primary key, Nama_Pelayan varchar(30) not null, Jenis_Kelamin char(1) CHECK(Jenis_Kelamin in ('L','P')))" +
                     "create table Stock_Masuk (Kode_Masuk char(6) not null primary key, Tanggal_Masuk date not null, Jumlah_Masuk varchar(30) not null, Stock_terbaru varchar(30), Kode_Obat char(6) foreign key references Obat(Kode_Obat) not null, Kode_Supplier char(6) foreign key references Supplier(Kode_Supplier))" +
                     "create table Stock_Keluar(Kode_Keluar char(6) not null primary key, Tanggal_Keluar date not null, Jumlah_Keluar varchar(30) not null, Saldo_Masuk money, Stock_Terbaru varchar(30) not null, Kode_Obat char(6) foreign key references Obat(Kode_Obat) not null, Kode_Pembeli char(6) foreign key references Pembeli(Kode_Pembeli))", con);
                 cm.ExecuteNonQuery();
@@ -38,9 +38,44 @@ namespace _20200140067_Tugas2_B
             }
             Console.ReadKey();
         }
+        public void IsiTabel()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-O4QM7APE;database=Apotek_Tiara067;Integrated Security = TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("insert into Supplier (Kode_Supplier, Nama_Supplier, No_Telpon, Alamat) values ('KS-102','Indah Sentosa Abadi','085332436766','Sumedang,Jawa Barat')" +
+                 "insert into Supplier (Kode_Supplier, Nama_Supplier, No_Telpon, Alamat) values ('KS-110','PT. Tifany UM','085677236526','Bandung,Jawa Barat')" +
+                 "insert into Supplier(Kode_Supplier, Nama_Supplier, No_Telpon, Alamat) values('KS-209', 'PT. Kalbe', '088987765567', 'Jakarta Selatan')" +
+                 "insert into Supplier (Kode_Supplier, Nama_Supplier, No_Telpon, Alamat) values ('KS-389','PT. Karunia Anugerah','089933422234','Surabaya,Jawa Timur')" +
+                 "insert into Supplier (Kode_Supplier, Nama_Supplier, No_Telpon, Alamat) values ('KS-500','Zaqquera','087653269992','Semarang,Jawa Tengah')"+
+                    "insert into Pelayan_Apotek (Kode_Pelayan, Nama_Pelayan, Jenis Kelamin) values ('KL-110','Asep Saepudin','L')"+
+                    "insert into Pelayan_Apotek (Kode_Pelayan, Nama_Pelayan, Jenis Kelamin) values ('KL-110','Asep Saepudin','L')"+
+                    "insert into Pelayan_Apotek (Kode_Pelayan, Nama_Pelayan, Jenis Kelamin) values ('KL-110','Asep Saepudin','L')"+
+                    "insert into Pelayan_Apotek (Kode_Pelayan, Nama_Pelayan, Jenis Kelamin) values ('KL-110','Asep Saepudin','L')"+
+                    "insert into Pelayan_Apotek (Kode_Pelayan, Nama_Pelayan, Jenis Kelamin) values ('KL-110','Asep Saepudin','L')", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Yeay!! Data berhasil ditambahkan!");
+                Console.ReadKey();
+            }catch(Exception e)
+            {
+                Console.WriteLine("Yeah.... Data gagal ditambahkan." + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
+            Console.ReadKey();
+        }
         static void Main(string[] args)
         {
-            new Program().BuatTable();
+            //new Program().BuatTable();
+            new Program().IsiTabel();
         }
     }
 }
